eec9d5bd97aa70c92bb9ca6268f4445861a45d5a
Kacper Farion <115004195+Terufloti@users.noreply.github.com>
2025-09-25 17:49:25 +0000

    Add defines

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,8 +3,21 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+#define CAP 975
+//#define DEBUG
 
 int main(){
+    int32_t small = CAP;
+    uint8_t large = 25;
+
+    #ifdef DEBUG
+        std::cout<<"[About to perform the addiction]"<<std::endl;
+    #endif
+    small += large;
+    std::cout<<small<<std::endl<<std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
