d9c504d12800880af016f63bc6a661d532610791
Kacper Farion <115004195+Terufloti@users.noreply.github.com>
2025-09-25 19:04:01 +0000

    Add structure (cows)

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -5,8 +5,24 @@
 #include <iostream>
 #include <string>
 
+enum class cow_purpose {dairy, meat, hide, pet};
+
+struct cow{
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+
 int main(){
-    
+    cow cow1;
+    cow1.age = 7;
+    cow1.name = "Jamie";
+    cow1.purpose = cow_purpose::pet;
+
+    std::cout<<"Cow specs:"<<std::endl;
+    std::cout<<cow1.name <<" is a type-"<<(int)cow1.purpose <<" cow."<<std::endl;
+    std::cout<<cow1.name <<" is "<<cow1.age<<" years old."<<std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
