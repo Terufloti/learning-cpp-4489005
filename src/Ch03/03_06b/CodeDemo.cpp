d9bf5c9d08621f22cea9a3a1d97c5fde29c3c9e0
Kacper Farion <115004195+Terufloti@users.noreply.github.com>
2025-10-01 20:02:37 +0000

    Add pointers operations in cow class

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -7,6 +7,17 @@
 
 int main(){
 
+    int b = 45;
+    int *ptr;
+
+    ptr = &b;
+
+    std::cout<<"The content of b is "<<b<<std::endl;
+    std::cout<<"ptr is pointing to address "<<ptr<<std::endl;
+    std::cout<<"Address of b is " << &b<<std::endl;
+    std::cout<<"Where ptr is pointing, we have " << *ptr <<std::endl;
+    std::cout<<"Address of ptr is "<<&ptr<<std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
