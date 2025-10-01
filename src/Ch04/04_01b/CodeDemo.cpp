3832066113608208567e00007005d58a687935cb
Kacper Farion <115004195+Terufloti@users.noreply.github.com>
2025-10-01 20:45:13 +0000

    Add if operations

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -5,7 +5,26 @@
 #include <iostream>
 
 int main(){
-    
+    int a = 2131;
+    bool flag = false;
+    char letter = 'a';
+
+    if (a > 2000){
+        std::cout<<"a > 2000"<<std::endl;
+    } else {
+        std::cout<<"a < 2000"<<std::endl;
+    }
+    if (a % 2) std::cout<<"a is odd";
+    else std::cout<<"a is even"<<std::endl;
+
+    std::cout<<"The letter "<<letter<<" is ";
+    if(letter != 'a' && letter != 'e' && letter != 'i' && letter != 'o' && letter != 'u' &&
+    letter != 'A' && letter != 'E' && letter != 'I' && letter != 'O' && letter != 'U') std::cout<<" not ";
+    std::cout<<"a vowel" <<std::endl;
+
+    if(flag) std::cout<<"Flag is true!"<<std::endl;
+    else std::cout<<"Flag is false!"<<std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
