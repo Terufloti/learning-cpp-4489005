4e54773d809a9ecf89e5bb6dd0a8e0303b57d083
Kacper Farion <115004195+Terufloti@users.noreply.github.com>
2025-10-01 20:12:53 +0000

    Add basic vectors operations

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -4,9 +4,22 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
+    std::vector<int> numbers;
+
+    numbers.push_back(2);
+    numbers.push_back(3);
+    numbers.push_back(4);
+    numbers.push_back(5);
+
+    std::cout<<"Vector numbers size = "<<numbers.size()<<std::endl;
+    std::cout<<"Vector numbers at index 2 = "<<numbers.at(2)<<std::endl;
+    numbers.at(2) = 13;
+    std::cout<<"Vector numbers at index 2 = "<<numbers.at(2)<<std::endl;
     
+
     std::cout << std::endl << std::endl;
     return (0);
 }
